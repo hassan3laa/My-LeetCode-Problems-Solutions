7355711d5ddc4c62007e21753b24ccb6884a158a
Hassan alaa <hassanalaa1400@gmail.com>
2026-01-05 02:09:46 +0200

    Time: 0 ms (100%), Space: 9.2 MB (58.11%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string ans = "";
+        while (i >= 0 || j >= 0 || carry) {
+            int d1 = (i >= 0) ? num1[i] - '0' : 0;
+            int d2 = (j >= 0) ? num2[j] - '0' : 0;
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            ans.push_back((sum % 10) + '0');
+            i--;
+            j--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
