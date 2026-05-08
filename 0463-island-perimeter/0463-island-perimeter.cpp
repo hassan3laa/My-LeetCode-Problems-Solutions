a422aa964fa8634bda936614c31fac8ee82361c1
Hassan alaa <hassanalaa1400@gmail.com>
2026-05-08 15:52:04 +0300

    Time: 21 ms (13.69%), Space: 108.8 MB (7.1%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size(), ans=0;
+    int dx[4]={1,-1,0,0};
+    int dy[4]={0,0,1,-1};
+
+    int islandPerimeter(vector<vector<int>>& a) {
+        int n=a.size();
+        int m=a[0].size();
+        vector<vector<int>>vis(n,vector<int>(m));
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    ans+=4;
-                    if(j-1 >= 0 and grid[i][j-1] == 1) ans-=2;
-                    if(i-1>= 0 and grid[i-1][j] == 1) ans-=2;
+                if(a[i][j] == 1){ // find first point to search
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    i=n;
+                    break;
                 }
             }
-        }   
+        }
+        int ans=0;
+        while(!q.empty()) { // bfs
+            auto [x,y]=q.front();
+            q.pop();
+            for(int d=0;d<4;d++){
+                int nx=x+dx[d];
+                int ny=y+dy[d];
+                if(nx<0 or ny<0 or nx>=n or ny>=m) ans++;
+                else if(a[nx][ny] == 0) ans++;
+                else if(!vis[nx][ny]){
+                    vis[nx][ny]=1;
+                    q.push({nx,ny});
+                }
+            }
+        }
         return ans;
     }
 };
