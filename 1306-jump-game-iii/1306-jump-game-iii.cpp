be400bfda4b276340f51f4b088c58d658b5251d7
Hassan alaa <hassanalaa1400@gmail.com>
2026-05-17 15:10:55 +0300

    Time: 55 ms (5.43%), Space: 54.9 MB (5.2%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>>adj;
-    vector<bool>vis;
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
-        adj.assign(n,vector<int>());
-        vis.assign(n,false);
-        for(int i=0;i<n;i++){
-            int left = (i- arr[i]), right= (i+arr[i]);
-            if(left >= 0) adj[i].push_back(left);
-            if(right < n) adj[i].push_back(right);
+        vector<vector<int>>adj(n);
+        for(int i=0; i<n; i++) {
+            int left= i-arr[i], right= i+arr[i];
+            if(left>=0){
+                adj[i].push_back(left);
+            }
+            if(right < n) {
+                adj[i].push_back(right);
+            }
         }
-        return dfs(start,arr,adj,vis);
-    }
-
-    bool dfs(int node, vector<int>&nums, vector<vector<int>>&adj, vector<bool>&vis) {
-        if(nums[node]==0) return true;
-        vis[node]=true;
-        for(auto v : adj[node]) {
-            if(!vis[v]) {
-                if(dfs(v, nums, adj, vis)){
-                    return true;
+        vector<bool>vis(n,false);
+        queue<int>q;
+        q.push(start);
+        vis[start]=true;
+        while(!q.empty()) {
+            int u=q.front();
+            q.pop();
+            if(arr[u] == 0) {
+                return true;
+            }
+            for(auto v:adj[u]) {
+                if(!vis[v]) {
+                    vis[v]=true;
+                    q.push(v);
                 }
             }
         }
