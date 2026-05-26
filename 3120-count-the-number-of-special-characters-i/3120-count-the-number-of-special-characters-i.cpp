5a3864a7815ef8de6bb5af7d071584990001b3e4
Hassan alaa <hassanalaa1400@gmail.com>
2026-05-26 13:52:18 +0300

    Time: 0 ms (100%), Space: 9.6 MB (19.49%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int numberOfSpecialChars(string s) {
-        unordered_set<char> st(s.begin(), s.end());
-        int ans=0;
-        for(char c='a'; c<='z'; c++){
-            if(st.count(c) and st.count(c-'a'+'A')){
-                ans++;
+        map<char,int>mp;
+        int ans=0, n=s.size();
+        for(int i=0;i<n;i++) {
+            mp[s[i]]++;
+        }
+        for(int i=0;i<n;i++) {
+            if(s[i] >= 'a' and s[i] <= 'z' and mp[s[i]]>0) {
+                if(mp[s[i]] >= 1 and mp[toupper(s[i])] >= 1) {
+                    ans++;
+                    mp[s[i]]=0;
+                }
             }
         }
         return ans;
