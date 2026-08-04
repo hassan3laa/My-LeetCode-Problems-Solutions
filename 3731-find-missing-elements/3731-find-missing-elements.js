7640c6c7ce001499cca734bc4ff3db933edb5975
Hassan alaa <hassanalaa1400@gmail.com>
2026-08-04 17:24:26 +0300

    Time: 6 ms (17.32%), Space: 58.7 MB (38.58%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.js
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -3,20 +3,12 @@
  * @return {number[]}
  */
 var findMissingElements = function(nums) {
-    const binarySearch = (nums,target) => {
-        let start=0,end=nums.length-1;
-        while(start<=end){
-            const mid=Math.floor((start+end)/2);
-            if(nums[mid]==target) return true;
-            else if(nums[mid]<target) start=mid+1;
-            else end=mid-1;
-        }
-        return false;
-    }
     nums.sort((a,b)=>a-b);
     let ans=[];
-    for(let x=nums[0];x<=nums[nums.length-1];x++){
-        if(!binarySearch(nums,x)) ans.push(x);
+    for(let i=1;i<nums.length;i++){
+        for(let x=nums[i-1]+1;x<nums[i];x++){
+            ans.push(x);
+        }
     }
     return ans;
 };
